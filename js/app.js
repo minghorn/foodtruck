20cbdb16c69aac20eafdf3273089d9ee5316d455
Gary <gary.thomas.ryan@gmail.com>
2013-11-10 12:08:09 -0800

    Update app.js
    Hope this helps!

--- js/app.js
+++ js/app.js
@@ -14,57 +14,61 @@ var app = function(data){
   function distance(a1, b1, a2, b2){
           var distance = Math.sqrt(((a1 - a2) * (a1 - a2)) + ((b1 - b2) * (b1 - b2)));
           return distance;
-  }
+  };
+
   function ltg(a,b){
     return a-b;
-  }        
+  };   
 
   function findNearest(lat, lon, amt){
           var nearest = [];
           var all = [];
           var hold = [];
           for (var i = 0; i < data.length; i++) {
-          if(data[i].longitude != undefined){
-                  all[i] = distance(data[i].longitude, data[i].latitude, lon, lat);
+            if(data[i].longitude != undefined){
+              all[i] = distance(data[i].longitude, data[i].latitude, lon, lat);
+            }
           }
-          };
           for(var m = 0; m < all.length; m++){
-                  hold[m] = all[m];
+            hold[m] = all[m];
           }
           hold.sort(ltg);
           for(var j = 0; j < amt; j++){
-                  for (var k = 0; k < all.length; k++) {
-                          if(hold[j] == all[k]){
-                                  nearest[nearest.length] = k;
-                          }
-                  };
-                  
+            for (var k = 0; k < all.length; k++) {
+              if(hold[j] == all[k]){
+                  nearest[nearest.length] = k;
+                }
+              };      
           }
-          return nearest;
-  }
-
-  function addMarker(lat, lon) {
-      var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(lat,lon),
-      });
-      for(var h = data.length-1; h>=0; h--){
-	  var infowindow = new google.maps.InfoWindow({
-	  content: contentString
-	  });
-
-	google.maps.event.addListener(marker, 'click', function() {
-	  infowindow.open(map,marker);
-	  });
-	};
-         marker.setMap(map);
-}
-
-  var ids = findNearest(data[1].latitude, data[1].longitude, 5);
-  for (var i = ids.length - 1; i >= 0; i--) {
-          addMarker(data[ids[i]].latitude, data[ids[i]].longitude);
-        var name = data[ids[i]].applicant;
-	  	var address = data[ids[i]].address;
-	  	var menu = data[ids[i]].fooditems;
-	  	var contentString = '<b>'+ name + '</b> <br/>'+ address + '<br/>' + menu;
+          var result = [];
+          nearest.forEach(function(value){
+            result.push(data[value]);
+          });
+          return result;
   };
+
+  function addMarker(lat, lon, contentString) {
+    var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(lat,lon),
+        map: map
+      });
+
+    var infoWindow = new google.maps.InfoWindow({
+      content: contentString
+    });
+
+	  google.maps.event.addListener(marker, 'click', function() {
+	    infoWindow.open(map,marker);
+	   });
+    console.log(lat, lon, contentString);
+  };
+
+  var nearestFoodTruckMarkers = findNearest(data[1].latitude, data[1].longitude, 5);
+  console.log('truck', nearestFoodTruckMarkers);
+
+  nearestFoodTruckMarkers.forEach(function(truckData){
+    var infoWindowContent = '<b>'+ truckData.applicant + '</b> <br/>'+ truckData.address + '<br/>' + truckData.foodItems;
+    addMarker(truckData.latitude, truckData.longitude, infoWindowContent);
+  });
+
 }
